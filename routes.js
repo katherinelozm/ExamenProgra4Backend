cc64546a78889a3f00cc4c025a86497ed66a5839
Katherine Lozano <katherinelozm@gmail.com>
2015-12-14 10:04:24 -0600

    Backend

--- routes.js
+++ routes.js
@@ -1,10 +1,10 @@
-var studentsController = require('./controllers/giftsController');
+var giftsController = require('./controllers/giftsController');
 var usersController = require('./controllers/usersController');
 var authController = require('./controllers/authController');
 
 exports.endpoints = [{method: 'GET', path: '/', config: {handler: function(request, reply){reply('API v1, Gifts')}}},
-	{method: 'GET', path: '/v1/gifts', config: studentsController.getStudents},
-  {method: 'POST', path: '/v1/gift', config: studentsController.createStudent},
+	{method: 'GET', path: '/v1/gifts', config: giftsController.getGifts},
+  {method: 'POST', path: '/v1/gift', config: giftsController.createGift},
 	{method: 'POST', path: '/v1/register', config: usersController.createUser},
 	{method: 'POST', path: '/v1/login', config: authController.login},
 	{method: 'GET', path: '/v1/logout', config: authController.logout}
